3b8b117bcea39b05fff6d4368ad83e91cae2b038
Jorn <Lotsch>
2022-03-22 10:42:54 +0100

    path adapted

--- Python/main.py
+++ Python/main.py
@@ -6,7 +6,7 @@ Created on Sun Feb 13 16:13:42 2022
 @author: joern
 """
 
-%cd "/home/joern/Aktuell/PaclitaxelPainLipidomics/08AnalyseProgramme/Python"
+%cd "/home/joern/Aktuell/PaclitaxelPainLipidomics/08AnalyseProgramme/PaclitaxelNeuropathyProject/Python"
 
 #import paclitaxel_exploreTukey
 
